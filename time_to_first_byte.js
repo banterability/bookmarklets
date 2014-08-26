7c452b51a2656aca5e38359328f42bc0144f7683
Jeffrey Long <jlong@groupon.com>
2014-08-26 17:47:01 -0500

    Can't link to bookmarklet in readme'

--- time_to_first_byte.js
+++ time_to_first_byte.js
@@ -6,3 +6,7 @@
     return alert('Timing data not available');
   }
 })();
+
+/* Compiled:
+javascript:(function(){try{var%20t=window.performance.timing;alert('Time%20to%20First%20Byte:%20'+(t.responseStart-t.fetchStart)+'%20ms')}catch(e){return%20alert('Timing%20data%20not%20available')}})();void(0);
+*/
